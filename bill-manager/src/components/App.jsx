8a7463d452f62c9203249819725af9f7459a59b4
Di Zhang <dizhang@dis-macbook-pro.usma.ibm.com>
2017-09-28 17:00:07 -0400

    second try

--- bill-manager/src/components/App.jsx
+++ bill-manager/src/components/App.jsx
@@ -1,32 +1,68 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { addReminder, deleteReminder, clearReminders } from '../actions';
+import { addReminder, retrieve, clearReminders } from '../actions';
 // import moment from 'moment';
 import Billrow from './billRow';
-
+import Summary from './summary'
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       amount: '',
       description: '',
-      total: 0
+      total: 0,
+      paidBy: '',
+      obligors: [],
+      d2RGU: 0,
+      d2EVA: 0,
+      RGU2EVA: 0
     }
   }
 
+  componentWillReceiveProps(nextProps) {
+    this.setState({d2RGU: nextProps.d2RGU});
+    this.setState({d2EVA: nextProps.d2EVA});
+    this.setState({RGU2EVA: nextProps.RGU2EVA});
+  }
+
+  retrieve() {
+    this.props.retrieve();
+  }
+
   clearState() {
+    this.setState({amount: ''})
+    this.setState({description: ''})
     document.getElementById('amount').value = '';
     document.getElementById('des').value = '';
   }
 
   addReminder() {
-    this.props.addReminder(this.state.amount, this.state.description);
+    if (!this.state.amount || this.state.obligors.length === 0) return;
+    this.props.addReminder(this.state.amount, this.state.description, this.state.paidBy, this.state.obligors);
     this.clearState();
   }
 
-  deleteReminder(id) {
-    this.props.deleteReminder(id);
+  isActive(value) {
+    return 'radio-group ' + ((value === this.state.paidBy) ? 'active' : '');
+  }
+
+  toggleMember(value) {
+    let newMember = [...this.state.obligors];
+    if (newMember.includes(value)) {
+      newMember.splice(newMember.indexOf(value), 1);
+    } else {
+     newMember.push(value);
+   }
+    this.setState({obligors: newMember});
+  }
+
+  isActiveObligor(value) {
+    return 'radio-group ' + ((this.state.obligors.includes(value)) ? 'active' : '');
+  }
+
+  switchPayer(name) {
+    this.setState({paidBy: name});
   }
 
   render() {
@@ -51,17 +87,52 @@ class App extends Component {
           </div>
           <button
             type="button"
-            className="btn btn-success"
+            className="btn btn-success marginLeft"
             onClick={() => this.addReminder()}
           >Add Records</button>
         </div>
-        <Billrow/>
-        <div
-          className="btn btn-danger"
-          onClick={() => this.props.clearReminders()}
-          >
-          Clear Records
+        <div className="form-group marginBoth5" id="D">
+          <div className="radio-group">
+            <div className="radio-group marginBoth5 memberTitle">PaidBy:</div>
+            <div className={this.isActive('D')} onClick={() => this.switchPayer('D')}>
+              <div className="marginBoth5">D</div>
+            </div>
+            <div className={this.isActive('RGU')} id="RGU">
+              <div className="marginBoth5" onClick={() => this.switchPayer('RGU')}>RGU</div>
+            </div>
+            <div className={this.isActive('EVA')} id="EVA">
+              <div className="marginBoth5" onClick={() => this.switchPayer('EVA')}>EVA</div>
+            </div>
+          </div>
+          <div className="radio-group">
+            <div className="radio-group marginBoth5 memberTitle">Obligor: </div>
+            <div className={this.isActiveObligor('D')} onClick={() => this.toggleMember('D')}>
+              <div className="marginBoth5">D</div>
+            </div>
+            <div className={this.isActiveObligor('RGU')} id="RGU">
+              <div className="marginBoth5" onClick={() => this.toggleMember('RGU')}>RGU</div>
+            </div>
+            <div className={this.isActiveObligor('EVA')} id="EVA">
+              <div className="marginBoth5" onClick={() => this.toggleMember('EVA')}>EVA</div>
+            </div>
+          </div>
         </div>
+        <Billrow/>
+        <div className="buttonGroup">
+          <div
+            className="btn btn-danger btncenter"
+            onClick={() => this.props.clearReminders()}
+            >
+            Clear Records
+          </div>
+          <div
+            className="btn btn-success marginLeft btncenter"
+            onClick={() => this.props.retrieve()}
+            >
+            Retrieve Records
+          </div>
+        </div>
+        <Summary/>
       </div>
     )
   }
@@ -69,12 +140,15 @@ class App extends Component {
 
 function mapStateToProps(state) {
   return {
-    bills: state.bills
+    bills: state.bills,
+    d2RGU: state.d2RGU,
+    d2EVA: state.d2EVA,
+    RGU2EVA: state.RGU2EVA
   }
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({addReminder, deleteReminder, clearReminders}, dispatch);
+  return bindActionCreators({addReminder, retrieve, clearReminders}, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
