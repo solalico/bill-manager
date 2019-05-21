195565e8045d8a8e8eae05a4578762c6258fd715
Di Zhang <Di.Zhang1@ibm.com>
2019-05-21 19:14:04 -0400

    1

--- bill-manager/src/firebase_credential.js
+++ bill-manager/src/firebase_credential.js
@@ -1,10 +1,11 @@
 const FIREBASE_CONFIG = {
-   apiKey: "AIzaSyDlRKZgitRpyIj8YeUVYJM7jJeRsvsbYtA",
-   authDomain: "bill-manager-c4b53.firebaseapp.com",
-   databaseURL: "https://bill-manager-c4b53.firebaseio.com",
-   projectId: "bill-manager-c4b53",
-   storageBucket: "",
-   messagingSenderId: "1029215998886"
+  apiKey: "AIzaSyDlRKZgitRpyIj8YeUVYJM7jJeRsvsbYtA",
+  authDomain: "bill-manager-c4b53.firebaseapp.com",
+  databaseURL: "https://bill-manager-c4b53.firebaseio.com",
+  projectId: "bill-manager-c4b53",
+  storageBucket: "bill-manager-c4b53.appspot.com",
+  messagingSenderId: "1029215998886",
+  appId: "1:1029215998886:web:173eee775fd4ccf5"
  };
 
 export default FIREBASE_CONFIG;
